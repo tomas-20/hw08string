7c4dbfd2cca7b7b07baee3d37105e3b01f526c1e
tomas-20 <tacuna20@stuy.edu>
2021-10-06 20:18:44 -0400

    created the mystrlen(char*) function

--- stringy.c
+++ stringy.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+int mystrlen(char *s) {
+  int length = 0;
+  for (char *cp = s; *cp; cp ++) {
+    length ++;
+  }
+  return length;
+}
 int main() {
+  printf("the length of 'hello' is %d\n", mystrlen("hello"));
   return 0;
 }
