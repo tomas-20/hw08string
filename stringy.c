b5d8959232bcf1214ab73767261c50374991d0b6
tomas-20 <tacuna20@stuy.edu>
2021-10-06 22:19:13 -0400

    added mystrncpy(char*, char*, int)

--- stringy.c
+++ stringy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 int mystrlen(char *s) {
   int length = 0;
   for (char *cp = s; *cp; cp ++) {
@@ -6,7 +7,33 @@ int mystrlen(char *s) {
   }
   return length;
 }
+char *mystrncpy(char *dest, char *source, int size) {
+  char *dp = dest;
+  char *sp = source;
+  for (int i = 0; i < size; i ++) {
+    *dp ++ = *sp ++;
+  }
+  return dest;
+}
 int main() {
+  printf("the length of 'hello' is %lu\n", strlen("hello"));
   printf("the length of 'hello' is %d\n", mystrlen("hello"));
+  char bob[10];
+  char joe[10];
+  strncpy(bob, "hello", 5);
+  mystrncpy(joe, "hello", 5);
+  printf("%s\n", bob);
+  printf("%s\n", joe);
+  for (int i = 0; i < 10; i ++) {
+    printf("%c %hhu\n", "hello"[i], "hello"[i]);
+  }
+  printf("\n");
+  for (int i = 0; i < 10; i ++) {
+    printf("%c %hhu\n", bob[i], bob[i]);
+  }
+  printf("\n");
+  for (int i = 0; i < 10; i ++) {
+    printf("%c %hhu\n", joe[i], joe[i]);
+  }
   return 0;
 }
