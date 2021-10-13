975b354120a1ab8107d09ab8ce0844ef56c616c5
tomas-20 <tacuna20@stuy.edu>
2021-10-13 18:27:54 -0400

    removed testing from stringy.c

--- stringy.c
+++ stringy.c
@@ -1,15 +1,4 @@
 #include <stdio.h>
-#include <string.h>
-void print_char(char c) {
-  printf("%c - %hhu\n", c, c);
-}
-void print_string(char *s, int n) {
-  char *cp = s;
-  for (int i = 0; i < n; i ++) {
-    print_char(*cp ++);
-  }
-  printf("\n");
-}
 int mystrlen(char *s) {
   int length = 0;
   while (*s) {
@@ -55,45 +44,4 @@ char *mystrchr(char *s, char c) {
     s ++;
   }
   return NULL;
-}
-void testcmp(char *a, char *b) {
-  printf("comparing %s and %s: %d %d\n", a, b, strcmp(a, b), mystrcmp(a, b));
-}
-void printpointer(char *p) {
-  printf("%p points to %c\n", p, *p);
-}
-int main() {
-  printf("the length of 'hello' is %lu\n", strlen("hello"));
-  printf("the length of 'hello' is %d\n", mystrlen("hello"));
-  char bob[10];
-  char joe[10];
-  mystrcpy(bob, "hello");
-  strcpy(joe, "hello");
-  printf("%s\n", bob);
-  printf("%s\n", joe);
-  char name[] = "bobby";
-  char greet0[50] = "hello my name is ";
-  char greet1[50] = "hello my name is ";
-  strcat(greet0, name);
-  mystrcat(greet1, name);
-  printf("%s\n", greet0);
-  printf("%s\n", greet1);
-  char empty0[50] = "";
-  char empty1[50] = "";
-  strcat(empty0, name);
-  mystrcat(empty1, name);
-  printf("%s\n", empty0);
-  printf("%s\n", empty1);
-  testcmp("apple", "orange");
-  testcmp("orange", "apple");
-  testcmp("apple", "apple");
-  testcmp("bob", "joe");
-  testcmp("joe", "bob");
-  testcmp("wab", "wabaloo");
-  testcmp("wabaloo", "wab");
-  char *pointer = strchr("hello", 'l');
-  char *apuntador = mystrchr("hello", 'l');
-  printpointer(pointer);
-  printpointer(apuntador);
-  return 0;
 }
